357eaf780fdd3a49b5aaa28015266da99e8f6670
Andrei Enshin <aenshin@c1r1s8.42tokyo.jp>
2024-10-20 00:14:38 +0900

    test swap

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 23:30:53 by aenshin           #+#    #+#             */
-/*   Updated: 2024/10/20 00:07:03 by aenshin          ###   ########.fr       */
+/*   Updated: 2024/10/20 00:14:29 by aenshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,34 @@
 //TODO parse args
 void main() {
 	t_node *lista;
+	t_node *listb;
 
 	lista = NULL;
+	listb = NULL;
+
 	add_node(&lista, 42);
-	add_node(&lista, 1);
-	add_node(&lista, 2);
+	add_node(&lista, 0);
+	add_node(&lista, 50);
 	print_list(lista);
 
-	swap(&lista);
-	print_list(lista);
-
-	printf("rm node with val [%d]\n", rm_node(&lista));
-	printf("rm node with val [%d]\n", rm_node(&lista));
-	printf("rm node with val [%d]\n", rm_node(&lista));
-	print_list(lista);
+	add_node(&listb, 1);
+	add_node(&listb, 2);
+	add_node(&listb, 3);
+	print_list(listb);
 
 	printf("swap\n");
+	swapab(&lista, &listb);
+	print_list(lista);
+	print_list(listb);
+
 	swapa(&lista);
+	print_list(lista);
+
+	swapb(&listb);
+	print_list(listb);
+
+	printf("rm node with val [%d]\n", rm_node(&lista));
+	printf("rm node with val [%d]\n", rm_node(&lista));
 	print_list(lista);
 }
 
