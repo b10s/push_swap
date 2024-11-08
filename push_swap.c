bc01eaf5dc7297f1fc2755e50a3e79671d698f9a
Andrei Enshin <aenshin@c1r7s2.42tokyo.jp>
2024-11-09 01:27:32 +0900

    norm

--- push_swap.c
+++ push_swap.c
@@ -1,25 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 01:21:54 by aenshin           #+#    #+#             */
+/*   Updated: 2024/11/09 01:26:50 by aenshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./push_swap.h"
 
-//TODO replace with my functions
-//TODO makefile
-//TODO norm
-int main(int argc, char *argv[]) {
-
+int	main(int argc, char *argv[])
+{
 	t_node	*list_a;
 	t_node	*list_b;
-	int			arr_len;
-	int			i;
-	int			*remap;
-	int			bits;
-	int	stack_b_len;
-	int	j;
-
+	int		arr_len;
+	int		i;
+	int		*remap;
+	int		bits;
+	int		stack_b_len;
+	int		j;
 
 	stack_b_len = 0;
 	list_a = NULL;
 	list_b = NULL;
 	arr_len = argc - 1;
-
 	if (argc < 2)
 		return (0);
 	if (args_are_valid(argc, argv) != 1)
@@ -27,11 +34,12 @@ int main(int argc, char *argv[]) {
 	remap = buble_sort(argc, argv);
 	if (remap == NULL)
 		print_err_and_exit();
-	for (i = 1; i < argc; i++)
-		if (remap[i-1] == remap[i])
+	i = 0;
+	while (++i < argc)
+		if (remap[i - 1] == remap[i])
 			print_err_and_exit();
 	if (sorted(argc, argv) == 1)
-		return 0;
+		return (0);
 	list_a = parse_args(argc, argv, remap);
 	if (argc - 1 <= 5)
 	{
@@ -39,22 +47,25 @@ int main(int argc, char *argv[]) {
 		return (0);
 	}
 	bits = calc_bits(argc - 1);
-	for (i = 0; i < bits; i++)
+	i = -1;
+	while (++i < bits)
 	{
 		stack_b_len = 0;
-		for (j = 0; j < argc - 1; j++)
+		j = -1;
+		while (++j < argc - 1)
 		{
-			if ( ((list_a->val >> i) & 1) == 0)
+			if (((list_a->val >> i) & 1) == 0)
 			{
 				push_b(&list_a, &list_b);
 				stack_b_len++;
-			} else
+			}
+			else
 				rotate_a(&list_a);
 		}
-		for (j = 0; j < stack_b_len; j++)
+		j = -1;
+		while (++j < stack_b_len)
 			push_a(&list_a, &list_b);
 	}
-
 	free_list(list_a);
 	free(remap);
 }
@@ -63,16 +74,17 @@ int	calc_bits(int len)
 {
 	int	res;
 	int	bits;
+
 	res = 2;
 	bits = 2;
-	for(;;)
+	while (1 == 1)
 	{
 		if (res == len)
-			return bits;
+			return (bits);
 		if (res > len)
-			return bits - 1;
+			return (bits - 1);
 		res = res * 2;
 		bits++;
 	}
-	return bits;
+	return (bits);
 }
