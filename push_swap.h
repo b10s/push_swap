8d14722d11f5d6652388c3c503bb01d3b89195e1
Andrei Enshin <aenshin@c1r7s2.42tokyo.jp>
2024-11-09 01:38:34 +0900

    nor

--- push_swap.h
+++ push_swap.h
@@ -1,15 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aenshin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 01:34:58 by aenshin           #+#    #+#             */
+/*   Updated: 2024/11/09 01:38:26 by aenshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct node {
-	int val;
-	struct node *next;
-	struct node *prev;
-} t_node;
+	int			val;
+	struct node	*next;
+	struct node	*prev;
+}	t_node;
 
-#define FT_FORWARD 1
-#define FT_REVERSE 0
+# define FT_FORWARD 1
+# define FT_REVERSE 0
 
 void	print_list(t_node *root);
 void	print_err_and_exit(void);
@@ -27,52 +42,20 @@ void	rotate_ab(t_node **listba, t_node **listb);
 void	reverse_rotate_a(t_node **lista);
 void	reverse_rotate_b(t_node **listb);
 void	reverse_rotate_ab(t_node **lista, t_node **listb);
-// TODO remove test?
-void	test();
 t_node	*parse_args(int argc, char *argv[], int *remap);
-int	list_len(t_node	*list);
+int		list_len(t_node	*list);
 void	sort_lists(t_node **list_a, t_node **list_b);
-int	need_swap(t_node *head, int direction);
-int	*buble_sort(int argc, char *argv[]);
+int		need_swap(t_node *head, int direction);
+int		*buble_sort(int argc, char *argv[]);
 void	print_arr(int *arr, int len);
 int		calc_bits(int len);
-int	sorted(int argc, char *argv[]);
-int	args_are_valid(int argc, char *argv[]);
+int		sorted(int argc, char *argv[]);
+int		args_are_valid(int argc, char *argv[]);
 long	ft_atoi(const char *str);
 void	simple_sort(t_node *list_a, t_node *list_b, int len);
-int	get_distance(t_node **stack, int index);
-int	is_sorted(t_node **stack);
+int		get_distance(t_node **stack, int index);
+int		is_sorted(t_node **stack);
 void	sort_5(t_node **stack_a, t_node **stack_b);
-void free_list(t_node *list);
+void	free_list(t_node *list);
 
-/*
-sa (swap a): Swap the first 2 elements at the top of stack a.
-Do nothing if there is only one or no elements.
-
-sb (swap b): Swap the first 2 elements at the top of stack b.
-Do nothing if there is only one or no elements.
-
-ss : sa and sb at the same time.
-
-pa (push a): Take the first element at the top of b and put it at the top of a.
-Do nothing if b is empty.
-
-pb (push b): Take the first element at the top of a and put it at the top of b.
-Do nothing if a is empty.
-
-ra (rotate a): Shift up all elements of stack a by 1.
-The first element becomes the last one.
-
-rb (rotate b): Shift up all elements of stack b by 1.
-The first element becomes the last one.
-
-rr : ra and rb at the same time.
-
-rra (reverse rotate a): Shift down all elements of stack a by 1.
-The last element becomes the first one.
-
-rrb (reverse rotate b): Shift down all elements of stack b by 1.
-The last element becomes the first one.
-
-rrr : rra and rrb at the same time.
-*/
+#endif
